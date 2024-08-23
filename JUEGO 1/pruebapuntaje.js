2774809160359d3a05de5b36ce355db9c4dc1f9d
micabod <167199490+micabod@users.noreply.github.com>
2024-08-23 10:47:12 -0300

    Update pruebapuntaje.js

--- JUEGO 1/pruebapuntaje.js
+++ JUEGO 1/pruebapuntaje.js
@@ -1,12 +1,12 @@
 import { readFileSync } from 'fs';
-const data = JSON.parse(readFileSync('palabras.json', 'utf8'));
+
+const datos = JSON.parse(readFileSync('palabras.json', 'utf8'));
+
 function obtenerpalabra_aleatoria(nivel) {
-    let palabras = data[`nivel_${nivel}`];
-    
-    // Declarar 'randomIndex' con 'let'
+    let palabras = datos[`nivel_${nivel}`];
     let randomIndex = Math.floor(Math.random() * palabras.length);
-    
-    // Retornar una palabra aleatoria
     return palabras[randomIndex];
-  }
-  
+}
+
+
+console.log(obtenerpalabra_aleatoria(1)); // devuelve una palabra aleatoria del nivel 1
