4c98f3c457734024ad3ea66e4a2cf04d105a6325
micabod <167199490+micabod@users.noreply.github.com>
2024-08-23 14:27:37 -0300

    empecé lo d los niveles

--- JUEGO 1/pruebapuntaje.js
+++ JUEGO 1/pruebapuntaje.js
@@ -1,12 +1,28 @@
 import { readFileSync } from 'fs';
 
 const datos = JSON.parse(readFileSync('palabras.json', 'utf8'));
+function preguntarnivel(){
+    let nivel = parseInt(prompt('¿Qué nivel quieres jugar del 1 al 3?'), 10);
 
-function obtenerpalabra_aleatoria(nivel) {
-    let palabras = datos[`nivel_${nivel}`];
-    let randomIndex = Math.floor(Math.random() * palabras.length);
-    return palabras[randomIndex];
+    while (nivel < 1 || nivel > 3 || isNaN(nivel)) {
+        nivel = parseInt(prompt('Ingrese un número válido entre 1 y 3:'), 10);
+    }
+    {
+    if nivel==1{
+
+        function obtenerpalabra_aleatoria(nivel) {
+        let palabras = datos[`nivel_${nivel}`];
+        let randomIndex = Math.floor(Math.random() * palabras.length);
+        return palabras[randomIndex];
+    }
+    {
+        else
+    
+    
+    console.log(obtenerpalabra_aleatoria(1)); // devuelve una palabra aleatoria del nivel 1
+    
 }
 
+solicitarNivel();
 
-console.log(obtenerpalabra_aleatoria(1)); // devuelve una palabra aleatoria del nivel 1
+
