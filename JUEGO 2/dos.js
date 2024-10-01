8bb62f74b1c4189a8fb3d454a1d1966a446973ad
WEBER JULIETA <49318078@ort>
2024-10-01 15:51:27 -0300

    ODIO PROYECTO CON MI VIDA

--- JUEGO 2/dos.js
+++ JUEGO 2/dos.js
@@ -1,4 +1,3 @@
-// Inicialización de variables
 let tarjetasdestapadas = 0;
 let tarjeta1 = null;
 let tarjeta2 = null;
@@ -8,20 +7,20 @@ let movimientos = 0;
 let aciertos = 0;
 let tiemporegresivoId = null;
 
-// Generación de números aleatorios
-let emociones = ['alegria', 'tristeza', 'enojo', 'desagrado', 'confusion', 'miedo'];
-let elementos = emociones.concat(emociones.map(e => `${e}.png`)); // Duplicar para tener pares, uno como palabra y otro como imagen
+
+let emociones = ['alegria', 'tristeza', 'enojo', 'desagrado', 'confusion', 'miedo', 'sorpresa', 'vergüenza'];
+let elementos = emociones.concat(emociones.map(e => `${e}.png`)); 
 
 elementos = elementos.sort(() => Math.random() - 0.5);
 console.log(elementos);
 
-// Función principal
+
 function destapar(id) {
     tarjetasdestapadas++;
     console.log(tarjetasdestapadas);
 
     if (tarjetasdestapadas == 1) {
-        // Mostrar primer número
+
         tarjeta1 = document.getElementById(id);
         primerresultado = elementos[id];
         if (primerresultado.endsWith('.png')) {
@@ -30,10 +29,9 @@ function destapar(id) {
             tarjeta1.innerHTML = primerresultado;
         }
 
-        // Deshabilitar primer botón
+
         tarjeta1.disabled = true;
     } else if (tarjetasdestapadas == 2) {
-        // Mostrar segundo número
         tarjeta2 = document.getElementById(id);
         segundoresultado = elementos[id];
         if (segundoresultado.endsWith('.png')) {
@@ -42,17 +40,14 @@ function destapar(id) {
             tarjeta2.innerHTML = segundoresultado;
         }
 
-        // Deshabilitar segundo botón
         tarjeta2.disabled = true;
 
-        // Incrementar movimientos
         movimientos++;
 
         if (primerresultado.replace('.png', '') == segundoresultado.replace('.png', '')) {
-            // Encerar contador tarjetas destapadas
+
             tarjetasdestapadas = 0;
 
-            // Aumentar aciertos
             aciertos++;
 
             if (aciertos == 6) {
@@ -60,7 +55,7 @@ function destapar(id) {
 
             }
         } else {
-            // Mostrar momentáneamente valores y volver a tapar
+
             setTimeout(() => {
                 tarjeta1.innerHTML = ' ';
                 tarjeta2.innerHTML = ' ';
