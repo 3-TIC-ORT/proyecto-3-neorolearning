87d6a8a62386f44d845376e3d02211cbc091d3bd
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2024-10-06 19:17:52 -0300

    vamos que se puede

--- JUEGO 1/uno.js
+++ JUEGO 1/uno.js
@@ -1,45 +1,67 @@
-let palabraArray = [];
-let palabra;
-let contador = 0;
-let divCuadrados = document.getElementById("cuadrados")
-let div = document.getElementById("divCajitas")
-function getPalabra(){
-    palabra = "brigitte";
-    for (let i = 0; i < palabra.length; i++) {
-        palabraArray.push(palabra[i]);
-        crearCuadrado()
-    }
-    magia()
-}
-
-function magia (){
-    let shuffle = palabraArray.sort((a, b) => 0.5 - Math.random());
-    console.log(shuffle)
-    for (let index = 0; index < shuffle.length; index++) {
-        crearCajitas(shuffle[index])        
-    }
-}
-
-function crearCajitas(data){
-    let cajita = document.createElement("h2");
-    cajita.addEventListener("click",()=>{
-        verificar(data)
-    })
-    cajita.setAttribute("class","cajita");
-    cajita.innerText = data;
-    div.appendChild(cajita)
-}
-
-function crearCuadrado(){
-    let cuadrado = document.createElement("h2");
-    cuadrado.setAttribute("class","cuadrado");
-    divCuadrados.appendChild(cuadrado)
-}
-
-function verificar (data){
-    if (data === palabraArray[0]) {
+let cajitas = document.getElementById('divCajitas');
+let letras = document.getElementById('divLetras');
+let correcto = document.getElementById("correcto")
+let img = document.getElementById("img")
+let word;
+let wordArray = [];
+let listaCajitas = [];
+let shuffleWord = [];
+const getWord = async () => {
+    word = "A lan";
     
+    for (let index = 0; index < word.length; index++) {
+        wordArray.push(word[index]);  
+        shuffleWord.push(word[index]);    
     }
-}
-getPalabra()
+    
+    shuffleWord = shuffleWord.sort((a, b) => 0.5 - Math.random())
 
+};
+
+const crearCajitas = async () => {
+    await getWord();
+    crearLetras();
+    wordArray.forEach((letter,i) => {
+        listaCajitas.push({index:i,letter:letter});
+        let div = document.createElement('h2');
+        div.setAttribute('id', `cajita${i}`);
+        div.classList.add('cajitas');
+        cajitas.appendChild(div);
+    });
+    console.log(listaCajitas);
+}
+
+
+const crearLetras = () => {
+    
+    shuffleWord.forEach((letter) => {
+        let h2 = document.createElement('h2');
+        h2.addEventListener('click', ()=>clickLetter(letter));
+        h2.classList.add('letras');
+        h2.innerHTML = letter;
+        letras.appendChild(h2);
+    });
+}
+
+const clickLetter = (letter) => {
+    console.log(letter);
+    img.src = "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/15665/production/_107435678_perro1.jpg.webp"
+        if(listaCajitas[0].letter === letter){
+            let div = document.getElementById(`cajita${listaCajitas[0].index}`);
+            div.innerHTML = letter;
+            listaCajitas.shift();
+            for (let index = 0; index < letras.children.length; index++) {
+                if(letras.children.item(index).innerHTML === letter){
+                    letras.children.item(index).remove();
+                    break;
+                }
+                
+            }
+            correcto.innerText = "Letra correcta"
+        }else{
+            correcto.innerText = "Letra incorrecta"
+        }
+    };
+
+
+crearCajitas();
---
+++ JUEGO 4/cuatro.js
@@ -0,0 +1,129 @@
+let patron,patronIngresado, score = 0, delay = 900
+//todas las variables con el mismo valor
+let record = 0
+let azul = document.getElementById("azul")
+let verde = document.getElementById("verde")
+let amarillo = document.getElementById("amarillo")
+let rojo = document.getElementById("rojo")
+const body = document.getElementById("body")
+const simonBody = document.getElementById("simon")
+const startButton = document.getElementById("start")
+const recordinging = document.getElementById("recordinging")
+const scoreHTML = document.getElementById("score")
+const perdisteMsg = document.getElementById("perdisteMsg")
+let bandera = 1
+
+
+function agregarColorPatron(){
+    nRandom = Math.ceil(Math.random()*4)
+    // rendondeo al n entero + cercano (entre 1 y 4)
+    patron.push(nRandom)
+    
+    score++;
+}
+async function singColor(color){
+    color.style.filter="brightness(150%)"
+    // iluminar el colorcito
+    await setTimeout(()=>{
+        color.style.filter="brightness(50%)"
+    },delay)
+    // par volverlo a poner como antes
+
+}
+
+async function multiColor(){
+    for(let i=0;i<patron.length;i++){
+        await setTimeout(()=>{
+            switch(patron[i]){
+                case 1:
+                    singColor(azul)
+                    break;
+                case 2:
+                    singColor(verde)
+                    break;
+                case 3:
+                    singColor(amarillo)
+                    break;
+                case 4:
+                    singColor(rojo)
+                    break;
+            }
+            // recorre el patron y muestra el color que hay que apretar
+        }, i*(delay+delay*.6))
+    }
+}
+azul.addEventListener("click", ()=>{
+    if(bandera === 0){
+        patronIngresado.push(1)
+        // vamos a ponerle el n1 a la lista del patron ingresado
+    }
+})
+verde.addEventListener("click", ()=>{
+    if(bandera === 0){
+    patronIngresado.push(2)
+        }// vamos a ponerle el n2 a la lista del patron ingresado
+})
+amarillo.addEventListener("click", ()=>{
+    if(bandera === 0){
+
+    patronIngresado.push(3)}
+        // vamos a ponerle el n3 a la lista del patron ingresado
+})
+rojo.addEventListener("click", ()=>{
+    if(bandera === 0){
+
+    patronIngresado.push(4)}
+        // vamos a ponerle el n4 a la lista del patron ingresado
+})
+async function comprobarArrays(arr, vuelta){
+    return new Promise(resolve => {
+        const checkLength = async () => {
+        while (arr.length !== vuelta+1) {
+            await new Promise(resolve => setTimeout(resolve, 10));
+        }
+        resolve();
+        };
+        checkLength();
+});
+}
+async function comprobarPatron(){
+    // ver si el nene lo hce bien
+    startButton.style.display="none"
+    // chau boton start
+    patronIngresado = []
+    for(let j = 0;j<patron.length;j++){
+        await comprobarArrays(patronIngresado, j)
+        // comprobar si cada elemento coincide
+        if(patron[j]!=patronIngresado[j]){
+            startButton.style.display="flex"
+            perdisteMsg.style.display="block"
+            return 0
+            // si sos medio loser y perdiste
+        }
+    }
+    setTimeout(()=>{
+        pasarNivel()
+    },800) // tiempo
+    // que muestre el siguiente color
+}
+
+startButton.addEventListener("click", empezar)
+async function empezar(){
+    patron = []
+    score = -1 // what
+    perdisteMsg.style.display="none"
+    // no mostrarlo siempre
+    agregarColorPatron()
+    await multiColor()
+    bandera = 0
+    comprobarPatron()
+}
+
+async function pasarNivel(){
+    bandera = 1
+    agregarColorPatron()
+    await multiColor()
+    comprobarPatron()
+}
+
+// siempre va a hacer estas 3 funciones 
